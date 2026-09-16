9d6539a9de5fadd6115ef361515828228fa11eda
Yu Osada <osada@shisui.tokyo>
2026-09-16 11:12:59 +0900

    メソッド作成

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 public class Main {
+    static int add(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args){
-        int[] scores = {80, 90, 70, 85, 95};
-
-        // for文
-        for(int i = 0; i < scores.length; i++){
-            System.out.println("Score " + (i + 1) + ": " + scores[i]);
-        }
-
-        // 拡張for文
-        for(int score : scores){
-            System.out.println("Score: " + score);
-        }
+        int result = add(5, 10);
+        System.out.println("The sum is: " + result);
     }
 }
